2d61f7c3a72c930d6a3d652af5e479ffd6b638ee
Bouma-Gregson <Keith.Bouma-Gregson@Waterboards.ca.gov>
2019-07-16 15:34:15 -0700

    Add batch file for R script

---
+++ Scripts/run_R_to_archive_data.bat
@@ -0,0 +1,9 @@
+:: This batch file runs the R script to archive HABs database data being sent to the Online Data Portal
+
+@ECHO OFF
+
+:: R script 
+"C:\Program Files\R\R-3.5.3\bin\Rscript" "C:\Users\KBouma-Gregson\Documents\FHABs_database\fhabs-database-archive\Scripts\join_db_versions.R"
+
+
+
